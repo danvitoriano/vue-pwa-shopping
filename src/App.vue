3d78ac144461d39179d2bed213e9f66a7acb2b31
geeomedina <geeomedina@hotmail.com>
2021-02-10 20:19:01 -0300

    Atividade 2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-
-      <v-btn
-        to="/"
-        text
-      >
+      <v-btn to="/" text>
         <span class="mr-2">Home</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-      <v-btn
-        to="/about"
-        text
-      >
+      <v-btn to="/about" text>
         <span class="mr-2">About</span>
         <v-icon>mdi-alert-decagram</v-icon>
+      </v-btn>
+      <v-btn to="/group4" text>
+        <span class="mr-2">Group 4</span>
+        <v-icon>mdi-border-all</v-icon>
       </v-btn>
     </v-app-bar>
 
