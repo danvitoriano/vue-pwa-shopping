6ae051b31ec3ee78b47fb7d0de150fc649248e1b
Helton Isac <helton.isac@gmail.com>
2021-02-11 08:16:48 -0300

    Creating and Using CreditCard component

---
+++ src/components/CreditCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="grupo3-form">
+    <h1>{{ "Cartão de Crédito" }}</h1>
+
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="creditNumber"
+        label="Número do Cartão"
+        v-mask="'#### #### #### ####'"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="name"
+        label="Nome no Cartão"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="date"
+        v-mask="'##/####'"
+        label="Data de validade"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="securityCode"
+        v-mask="'###'"
+        label="Código de segurança"
+        required
+      ></v-text-field>
+
+      <v-btn
+        block
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="validate"
+      >
+        Cadastrar Cartão
+      </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    creditNumber: "",
+    name: "",
+    date: "",
+    securityCode: "",
+  }),
+};
+</script>
+
+
+<style>
+.grupo3-form {
+  margin: auto;
+  max-width: 500px;
+  padding: 40px;
+}
+
+.mr4 {
+  flex: 1;
+}
+</style>
--- src/views/Grupo3.vue
+++ src/views/Grupo3.vue
@@ -1,41 +1,13 @@
 <template>
-  <div>
-    <v-form v-model="valid">
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field
-              v-model="firstname"
-              :rules="nameRules"
-              :counter="10"
-              label="First name"
-              required
-            ></v-text-field>
-          </v-col>
-
-          <v-col cols="12" md="4">
-            <v-text-field
-              v-model="lastname"
-              :rules="nameRules"
-              :counter="10"
-              label="Last name"
-              required
-            ></v-text-field>
-          </v-col>
-
-          <v-col cols="12" md="4">
-            <v-text-field
-              v-model="email"
-              :rules="emailRules"
-              label="E-mail"
-              required
-            ></v-text-field>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-    <div class="text-center">
-      <v-pagination v-model="page" :length="3"></v-pagination>
-    </div>
-  </div>
+  <credit-card />
 </template>
+
+<script>
+import CreditCard from "../components/CreditCard.vue";
+export default {
+  components: { CreditCard },
+};
+</script>
+
+<style>
+</style>
