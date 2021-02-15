b2394cec732cd09fab2c53ce83ae29978045b2ba
hique023 <pedroviski10@hotmail.com>
2021-02-15 10:47:08 -0300

    Adicionado componente Clima e configurado template

---
+++ src/components/Clima.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="clima">
+    <p>Pesquisa de Clima</p>
+    <input type="text" class="city-input" v-model="city" placeholder="Cidade" maxlength="8" />
+    <button type="button" class="city-button" v-on:click="searchCity">Pesquisar</button>
+  </div>
+</template>
+
+<script>
+
+// import axios from "axios"; // biblioteca usada para fazer requisições HTTP, similar ao Fetch
+
+export default {
+    name: "Clima",
+    data() {
+        return {
+            city: '',
+            dataClima: {},
+        }
+    },
+    methods: {
+        searchCity: function () {
+            if (this.city.length > 0) {
+                console.log(`${this.city}`);
+            }
+        }
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style>
+
+.city-input {
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 5px;
+    width: 250px;
+    margin: 10px;
+}
+
+.city-button {
+    border-radius: 5px;
+    color: white;
+    margin: 10px;
+}
+
+.city-button:hover {
+    background-color: rgb(155, 155, 155)
+}
+
+</style>
---
+++ src/views/Clima.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      shrink-on-scroll
+    >
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+      <v-toolbar-title>Pesquisa de Clima</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+
+      <!-- <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+      <clima></clima>
+        <v-row>
+          <v-col
+            v-for="n in 3"
+            :key="n"
+            cols="4"
+          >
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+import Clima from '../components/Clima.vue'
+
+  export default {
+    name: "Clima",
+    data() {
+      return {
+      }
+    },
+    components: {
+      clima: Clima
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+  }
+</script>
