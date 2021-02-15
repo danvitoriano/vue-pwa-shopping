bb47199c3bac7d9a80b6ef5a9883a51f8f89807e
hique023 <pedroviski10@hotmail.com>
2021-02-15 16:53:28 -0300

    Alterado variáveis

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,15 +1,11 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      shrink-on-scroll
-    >
+    <v-app-bar app shrink-on-scroll>
       <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
 
       <v-toolbar-title>Pesquisa de Notícias</v-toolbar-title>
 
       <v-spacer></v-spacer>
-
 
       <!-- <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
@@ -18,13 +14,9 @@
 
     <v-main>
       <v-container>
-      <news></news>
+        <news></news>
         <v-row>
-          <v-col
-            v-for="n in 3"
-            :key="n"
-            cols="4"
-          >
+          <v-col v-for="n in 15" :key="n" cols="4">
             <v-card height="200"></v-card>
           </v-col>
         </v-row>
@@ -34,23 +26,17 @@
 </template>
 
 <script>
+import News from "../components/News.vue";
 
-import News from '../components/News.vue'
-
-  export default {
-    name: "News",
-    data() {
-      return {
-      }
-    },
-    components: {
-      news: News
-    },
-    methods: {
-
-    },
-    computed: {
-
-    }
-  }
+export default {
+  name: "News",
+  data() {
+    return {};
+  },
+  components: {
+    news: News,
+  },
+  methods: {},
+  computed: {},
+};
 </script>
