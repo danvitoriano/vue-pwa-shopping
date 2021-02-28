c9cad2a5c9ff224cd6b7acdee9c0bc69f20df050
hique023 <pedroviski10@hotmail.com>
2021-02-28 16:55:41 -0300

    Removido comentário do teste

--- tests/e2e/specs/news.js
+++ tests/e2e/specs/news.js
@@ -3,7 +3,6 @@
 describe("Validação de busca de notícia", () => {
     it("Visits the app root url", () => {
         cy.visit("/news");
-        // cy.contains("v-toolbar-title", "Feed de Notícias");
         cy.get('#input-subject').type('soja')
         cy.get('#search-news').click()
     });
