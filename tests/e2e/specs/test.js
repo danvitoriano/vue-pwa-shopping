4b246e4389b8e73a27abb405ed90b0313661be77
Lyan Masterson Pereira Miguel <lyan_master@hotmail.com>
2021-03-04 18:59:53 -0300

    Rebase base test file.

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("The Home Page", () => {
-  it("successfully loads", () => {
+describe("My First Test", () => {
+  it("Visits the app root url", () => {
     cy.visit("/");
+    cy.contains("h1", "Welcome to Your Vue.js App");
   });
 });
